dfb1ca359d23f77cfd7107030bc3a1ca17f8b3f2
David Rivadeneira <migueldavidrivadeneira@gmail.com>
2025-06-21 03:00:39 -0300

    Subo cambios hechos en el crud y la instalacion de axios, también el archivo de db.json

---
+++ Defensa-carpetas/src/Pages/CreatePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import EditTable from '../components/CRUD/EditTable'
+import Footer from '../components/Footer'
+
+const CreatePage = () => {
+  return (
+    <div>
+        <Header/>
+        <EditTable/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default CreatePage

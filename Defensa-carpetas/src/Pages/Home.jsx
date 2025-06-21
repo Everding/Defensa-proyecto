106ae3f882ba04af8c1ffafd3c077f98dc2716a7
David Rivadeneira <migueldavidrivadeneira@gmail.com>
2025-06-21 01:20:40 -0300

    Componentes inicializados y exportados, el home ya cuenta con los componentes en su div

--- Defensa-carpetas/src/Pages/Home.jsx
+++ Defensa-carpetas/src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Main from '../components/Main'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
   )
 }
 
